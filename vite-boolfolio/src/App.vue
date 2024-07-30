24a2a4984a2915407b7b5fcd2aa767256599a47c
federicoalagna <federicoalagna01@gmail.com>
2024-07-30 16:55:52 +0200

    creazione di una project card per l'eventuale chiamata api e modifica del file app.vue

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,30 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Progetti</h1>
+    <div v-if="loading">Caricamento...</div>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="projects.length">
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import axios from './axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProjectCard
+  },
+  data() {
+    return {
+      projects: [],
+      loading: true,
+      error: null
+    };
+  },
+  mounted() {
+    axios.get('/projects')
+      .then(response => {
+        this.projects = response.data;
+        this.loading = false;
+      })
+      .catch(error => {
+        this.error = 'Errore durante il caricamento dei progetti';
+        this.loading = false;
+      });
+  }
+};
+</script>
---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="project-card">
+      <h2>{{ project.titolo }}</h2>
+      <p>{{ project.descrizione }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProjectCard',
+    props: {
+      project: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .project-card {
+    border: 1px solid #ccc;
+    padding: 16px;
+    margin: 16px;
+    border-radius: 8px;
+  }
+  </style>
+  
